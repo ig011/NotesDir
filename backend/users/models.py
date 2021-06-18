b82996dbb12086e696818312649743586f4a65c4
ig011 <roggemaster@gmail.com>
2021-06-19 00:28:35 +0200

    Add custom User extension for authentication

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=200, blank=False, verbose_name='email')
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
