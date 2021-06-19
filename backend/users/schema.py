c62f341d0840b0794eaed4cbf8513bde0bb5e79f
ig011 <roggemaster@gmail.com>
2021-06-19 02:16:50 +0200

    Add account verification

--- backend/users/schema.py
+++ backend/users/schema.py
@@ -4,6 +4,7 @@ from graphql_auth import mutations
 
 class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
+    verify_account = mutations.VerifyAccount.Field()
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
     pass
