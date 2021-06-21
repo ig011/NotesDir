49fcd72439fe1cd3c8bdc1e519a1317164c10069
ig011 <roggemaster@gmail.com>
2021-06-21 17:07:49 +0200

    Add frontend using next.js

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Navbar from "../components/Navbar/Navbar";
+import styles from "../styles/home/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>NotesDir</title>
+        <meta name="description" content="NotesDir WebApp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+    </div>
+  );
+}
