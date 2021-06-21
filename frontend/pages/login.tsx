6682f107f8610b359bc868623fa772e247ce7e0a
ig011 <roggemaster@gmail.com>
2021-06-21 17:56:52 +0200

    Create LoginForm

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/home/Home.module.css";
+
+function Login() {
+  return <div className={styles.container}>TEST</div>;
+}
+
+export default Login;
