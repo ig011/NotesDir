0a1e64fd8283f6323a0183fd9f6c9d3f463a7ea3
ig011 <roggemaster@gmail.com>
2021-06-29 18:04:22 +0200

    Test

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/home/Home.module.css";
 
 function Login() {
-  return <div className={styles.container}>TEST</div>;
+  return <div className={styles.container}>TESTs</div>;
 }
 
 export default Login;
