91579a77f0e68ca0f2e52fbb9ab64e0a7abffab2
ig011 <roggemaster@gmail.com>
2021-06-30 17:55:20 +0200

    Change datetime

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-from datetime import datetime
 from users.models import ExtendUser
+from django.utils.timezone import now
 
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=250, blank=False)
     description = models.TextField()
-    modified_at = models.DateTimeField(default=datetime.now())
+    modified_at = models.DateTimeField(default=now())
     created_at = models.DateTimeField(auto_now_add=True)
     thumbnail = models.URLField(blank=True, null=True)
     background_color = models.CharField(max_length=250, blank=True, null=True, default='white')
-    start_date = models.DateTimeField(blank=True, null=True, default=datetime.now())
-    end_date = models.DateTimeField(default=datetime.now())
+    start_date = models.DateTimeField(blank=True, null=True, default=now())
+    end_date = models.DateTimeField(default=now())
     user = models.ForeignKey(ExtendUser, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=250, unique=True)
     is_done = models.BooleanField(default=False)
