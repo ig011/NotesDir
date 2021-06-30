22af6624fa0413c38f21041cd35108183c3b09b3
ig011 <roggemaster@gmail.com>
2021-06-30 23:00:14 +0200

    Create HomePage

---
+++ frontend/components/Home/TodosContainer/TodosContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./TodosContainer.module.css";
+
+function TodosContainer() {
+  return <div className={styles.container}>TODO LIST</div>;
+}
+
+export default TodosContainer;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Topbar from "../components/Home/Top-bar/Topbar";
+import TodosContainer from "../components/Home/TodosContainer/TodosContainer";
 import styles from "../styles/home/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Topbar />
+      <TodosContainer />
     </div>
   );
 }
