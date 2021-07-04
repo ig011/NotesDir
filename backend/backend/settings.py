1dd3a016146f1ae5c1be8df7b8c5444cdec9b3c1
ig011 <roggemaster@gmail.com>
2021-07-04 02:00:03 +0200

    Create navbar after log in user

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,7 +151,7 @@ GRAPHQL_JWT = {
 }
 
 GRAPHQL_AUTH = {
-    'ALLOW_LOGIN_NOT_VERIFIED': False,
+    'ALLOW_LOGIN_NOT_VERIFIED': True,
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./Navbar.module.css";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { deepPurple } from "@material-ui/core/colors";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+    purple: {
+      color: theme.palette.getContrastText(deepPurple[500]),
+      backgroundColor: deepPurple[500],
+    },
+  })
+);
 
 function Navbar() {
+  const [isLogged, setIsLogged] = useState(true);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const classes = useStyles();
+
   return (
     <header className={styles.container}>
       <div className={styles.logo}>NotesDir</div>
@@ -18,13 +40,44 @@ function Navbar() {
         </Link>
       </div>
       <div className={styles.account}>
-        <Link href="/signup">
-          <button className={styles.btn1}>Sign up</button>
-        </Link>
-        <Link href="/signin">
-          <button className={styles.btn1}>Sign in</button>
-        </Link>
+        {!isLogged ? (
+          <>
+            <Link href="/signup">
+              <button className={styles.btn1}>Sign up</button>
+            </Link>
+            <Link href="/signin">
+              <button className={styles.btn1}>Sign in</button>
+            </Link>
+          </>
+        ) : (
+          <div className={classes.root}>
+            <Avatar className={classes.purple}>U</Avatar>
+            <button
+              className={styles.btn2}
+              onMouseEnter={() => setShowAccountMenu(true)}
+              onMouseLeave={() => setShowAccountMenu(false)}
+            >
+              account -
+            </button>
+          </div>
+        )}
       </div>
+      {showAccountMenu && (<div className={styles.accountmenucontainer}>
+        <div className={styles.element}>
+          <Link href="/">
+            <a>My account</a>
+          </Link>
+          <Link href="/">
+            <a>Todos</a>
+          </Link>
+          <Link href="/">
+            <a>Settings</a>
+          </Link>
+          <Link href="/">
+            <a>Sign out</a>
+          </Link>
+        </div>
+      </div>}
     </header>
   );
 }
