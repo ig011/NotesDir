2f5743675c233d915d00a6e755a44108dc491c9c
ig011 <roggemaster@gmail.com>
2021-07-30 18:11:38 +0200

    Home router push

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Topbar from "../components/Home/Top-bar/Topbar";
 import TodosContainer from "../components/Home/TodosContainer/TodosContainer";
 import styles from "../styles/home/Home.module.css";
 import { useRouter } from "next/router";
-import { GET_CURRENT_USER, UserInfo } from "./api/apollo-client";
+import { GET_CURRENT_USER, UserInfo, client } from "./api/apollo-client";
 import { useContainer } from "unstated-next";
-import { useQuery } from "@apollo/client";
+import { isNonNullType } from "graphql";
 
 export default function Home() {
   const { isLogged, changeIsLogged, changeUsername } = useContainer(UserInfo);
   const router = useRouter();
 
-  const isUserLogged = useQuery(GET_CURRENT_USER);
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      await client
+        .query({ query: GET_CURRENT_USER })
+        .then((response) => {
+          console.log(response.data);
+          if (response.data?.me === null) {
+            router.push({ pathname: "signin" });
+          }
+        })
+        .catch();
+    };
+    getCurrentUser();
+  }, []);
 
   return (
     <>
